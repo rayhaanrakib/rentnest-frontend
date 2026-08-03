cc870771d7ae3296be69ee15b58c78670a1aafd4
R I Rayhan <islamraihanofficial@gmail.com>
2026-08-03 16:41:09 +0600

    feat(middleware): implement proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,119 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { JwtPayload } from "jsonwebtoken";
+import { cookies } from "next/headers";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { getNewAccessToken } from "./service/refreshToken";
+import { jwtUtils } from "./utils/jwt";
 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+const AUTH_ROUTES = ["/login", "/register"];
+const PAYMENT_ROUTES = ["/payment", "/payment/success", "/payment/cancel"];
+const PUBLIC_ROUTES = ["/", "/properties", "/categories","/login", "/register"];
+
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const cookieStore = await cookies();
+
+  let accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
+
+  let decodedAccessToken = accessToken
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
+    : null;
+
+  const decodedRefreshToken = refreshToken
+    ? jwtUtils.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+    : null;
+
+  const response = NextResponse.next();
+
+  if (!decodedAccessToken?.success && decodedRefreshToken?.success) {
+    const result = await getNewAccessToken();
+
+    if (result.success) {
+      const newAccessToken = result.data.accessToken;
+
+      response.cookies.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+
+      accessToken = newAccessToken;
+      decodedAccessToken = jwtUtils.verifyToken(
+        accessToken!,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
+  }
+
+  let userRole: string | null = null;
+
+  if (!decodedAccessToken?.success) {
+    response.cookies.delete("accessToken");
+    cookieStore.delete("accessToken");
+  }
+
+  if (decodedAccessToken?.success && decodedAccessToken.data) {
+    userRole = (decodedAccessToken.data as JwtPayload).role;
+  }
+
+  // Logged-in users are redirect
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "TENANT") {
+      return NextResponse.redirect(new URL("/tenant/dashboard", request.url));
+    } else if (userRole === "LANDLORD") {
+      return NextResponse.redirect(new URL("/landlord/dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  const isPaymentRoute = PAYMENT_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  if (accessToken && isPaymentRoute) {
+    return response;
+  }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  // Authentication
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("callbackUrl", pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  // Authorization: role-based
+  if (pathname.startsWith("/tenant/dashboard") && userRole !== "TENANT") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (
+    pathname.startsWith("/landlord/dashboard") &&
+    userRole !== "LANDLORD"
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (pathname.startsWith("/admin/dashboard") && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
+
+  return response;
 }
 
 export const config = {
-  matcher: '/about/:path*',
-}
+  matcher: ["/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)"],
+};
